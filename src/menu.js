f9f95a9f5f42efa90a35368b996da980db67ceee
minho-sama <n.a.minh1106@gmail.com>
2021-04-09 20:37:51 +0200

    proba3

--- src/menu.js
+++ src/menu.js
@@ -2,28 +2,28 @@ function loadMenu(){
     const menu = document.createElement('div')
     menu.classList.add('menu')
     menu.innerHTML = `
-    <img src = '../pesto/pesto1.jpg'>
+    <img src = '../dist/pesto/pesto1.jpg'>
     <label id = 'item1'>
     <p id = 'name'> Pesto </p>
         <p id = 'caption'> The authentic, creamy pesto made with fresh basil and love</p>
         <p class = 'price'> 1390 HUF </p>
     </label>
 
-    <img src = '../pesto/pesto2.jpg'>
+    <img src = '../dist/pesto/pesto2.jpg'>
     <label id = 'item2'>
         <p id = 'name'> Pesto Deluxe </p>
         <p id = 'caption'> Premium pesto, for premium people </p>
         <p class = 'price'> 2190 HUF </p>
     </label>
 
-    <img src = '../pesto/pesto3.jpg'>
+    <img src = '../dist/pesto/pesto3.jpg'>
     <label id = 'item3'>
         <p id = 'name'> Pesto Deluxe </p>
         <p id = 'caption'> Home-made pesto pasta </p>
         <p class = 'price'> 990 HUF</p>
     </label>
 
-    <img src = '../pesto/pesto4.jpg'>
+    <img src = '../dist/pesto/pesto4.jpg'>
     <label id = 'item4'>
         <p id = 'name'> Red Pesto </p>
         <p id = 'caption'> Sun-dried tomato pesto from Italy</p>
